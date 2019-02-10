a8a950098d1960e1668787618ec37bd36e4f7722
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-02-10 05:38:06 -0500

    Fixed inverseTangent and wrote a test case for it

--- src/test/testInverseTangent.java
+++ src/test/testInverseTangent.java
@@ -1,2 +1,20 @@
+import calculations.Calculations;
+import calculations.InverseTangent;
+import calculations.Subtract;
+import calculations.Value;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class testInverseTangent {
+    @Test
+    public void testInverseTangent() {
+        // Given
+        Calculations testValue1 = new Value(45);
+        // When
+        InverseTangent testInverseTangent = new InverseTangent(testValue1);
+        // Then
+        float expectedResult = 1.5485777f;
+        float actualResult = testInverseTangent.evaluate();
+        Assert.assertEquals(expectedResult, actualResult, 0.01);
+    }
 }
